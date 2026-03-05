73a17642fd030af733fedfa108471cc2520ee6f4
kipishbringer <kipishbringer@gmail.com>
2026-03-05 07:18:24 +0000

    deployin#1

--- config/settings.py
+++ config/settings.py
@@ -119,14 +119,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-if DEBUG :
-    STATICFILES_DIRS = (
-     os.path.join(BASE_DIR, 'static'),
- )
+if DEBUG:
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 else:
-    STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT =  os.path.join(BASE_DIR, 'static/')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
